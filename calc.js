ec0d0cf6f6ea346126245e5cfa3faa91ffa5acb0
Andrew Stubbs <andrew.stubbs@gmail.com>
2017-11-28 17:42:25 +0000

    Initial bitsize support.
    Also:
     - add rows without wiping content.
     - Access object properties more directly.

--- calc.js
+++ calc.js
@@ -1,17 +1,8 @@
 var template='\
-  <tr id="newrow" class="calcrow"> \n\
     <td> \n\
-      <p class="label"></p> \n\
-      <p class="sizepara"> \n\
-        Size: \n\
-        <select class="size"> \n\
-          <option value="8 bit">8 bit</option> \n\
-	  <option value="16 bit">16 bit</option> \n\
-	  <option value="32 bit">32 bit</option> \n\
-	  <option value="64 bit">64 bit</option> \n\
-	</select> \n\
-      </p> \n\
+      <p class="labelpara"></p> \n\
       <p class="oppara"></p> \n\
+      <p class="sizepara"></p> \n\
       <p> \n\
         <button class="deletebutton">Delete</button> \n\
       </p> \n\
@@ -20,19 +11,19 @@ var template='\
       <table class="convtable"> \n\
 	<tr> \n\
 	  <td>Hexadecimal:</td> \n\
-	  <td><input onInput="hexchanged(this)" placeholder="0" class="hexbox"></input></td> \n\
+	  <td><input onInput="intchanged(this, 16)" placeholder="0" class="hexbox"></input></td> \n\
 	</tr> \n\
 	<tr> \n\
 	  <td>Octal:</td> \n\
-	  <td><input onInput="octchanged(this)" placeholder="0" class="octbox"></input></td> \n\
+	  <td><input onInput="intchanged(this, 8)" placeholder="0" class="octbox"></input></td> \n\
 	</tr> \n\
 	<tr> \n\
 	  <td>Signed Decimal:</td> \n\
-	  <td><input onInput="decchanged(this)" placeholder="0" class="sdecbox"></input></td> \n\
+	  <td><input onInput="intchanged(this, 10)" placeholder="0" class="sdecbox"></input></td> \n\
 	</tr> \n\
 	<tr> \n\
 	  <td>Unsigned Decimal:</td> \n\
-	  <td><input onInput="decchanged(this)" placeholder="0" class="udecbox"></input></td> \n\
+	  <td><input onInput="intchanged(this, 10)" placeholder="0" class="udecbox"></input></td> \n\
 	</tr> \n\
 	<tr> \n\
 	  <td>Float32:</td> \n\
@@ -40,7 +31,7 @@ var template='\
 	</tr> \n\
 	<tr> \n\
 	  <td>Binary:</td> \n\
-	  <td><input onInput="binchanged(this)" placeholder="0" class="binbox"></input></td> \n\
+	  <td><input onInput="intchanged(this, 2)" placeholder="0" class="binbox"></input></td> \n\
 	</tr> \n\
       </table> \n\
     </td> \n\
@@ -48,70 +39,84 @@ var template='\
       <input type="radio" class="leftop">Left Operand</input> \n\
       <input type="radio" class="rightop">Right Operand</input> \n\
     </td> \n\
-  </tr> \n\
 ';
+var elements = ["labelpara", "sizepara", "hexbox", "octbox", "sdecbox",
+		"udecbox", "floatbox", "binbox"];
 
 var update = 0;
 var calculatorui = document.getElementById("calculator");
 var rowcount = 0;
 var rowarray = [];
+var optab = [];
 
-function findrow (elm) {
-  while ((elm = elm.parentElement) && !elm.classList.contains("calcrow"));
-  return elm;
-}
-
-function writevalues (row, hexvalue, isFloat) {
+function writevalues32 (row, svalue, isFloat) {
   var intvalue = new Int32Array(1);
   var uintvalue = new Uint32Array(intvalue.buffer);
   var floatvalue = new Float32Array(intvalue.buffer);
 
   if (isFloat)
-    floatvalue[0] = hexvalue;
+    floatvalue[0] = svalue;
   else
-    intvalue[0] = hexvalue;
+    intvalue[0] = svalue;
 
-  row.querySelector(".hexbox").value = uintvalue[0].toString(16);
-  row.querySelector(".octbox").value = uintvalue[0].toString(8);
-  row.querySelector(".sdecbox").value = intvalue[0].toString(10);
-  row.querySelector(".udecbox").value = uintvalue[0].toString(10);
-  row.querySelector(".floatbox").value = floatvalue[0].toString();
-  row.querySelector(".binbox").value = uintvalue[0].toString(2);
+  row.hexbox.value = uintvalue[0].toString(16);
+  row.octbox.value = uintvalue[0].toString(8);
+  row.sdecbox.value = intvalue[0].toString(10);
+  row.udecbox.value = uintvalue[0].toString(10);
+  row.floatbox.value = floatvalue[0].toString();
+  row.binbox.value = uintvalue[0].toString(2);
 }
 
-function hexchanged (box) {
-  var row = findrow (box);
-  writevalues (row, parseInt(box.value, 16), false);
-}
+optab[3] = {
+  writevalues: writevalues32
+};
 
-function octchanged (box) {
-  var row = findrow (box);
-  writevalues (row, parseInt(box.value, 8), false);
-}
-
-function decchanged (box) {
-  var row = findrow (box);
-  writevalues (row, parseInt(box.value, 10), false);
+function intchanged (box, radix) {
+  var op = box.row.size;
+  optab[op].writevalues (box.row, parseInt(box.value, radix), false);
 }
 
 function floatchanged (box) {
-  var row = findrow (box);
-  writevalues (row, box.value, true);
+  var op = box.row.size;
+  optab[op].writevalues (box.row, box.value, true);
 }
 
-function binchanged (box) {
-  var row = findrow (box);
-  writevalues (row, parseInt(box.value, 2), false);
+function rowname (index) {
+  var name = "";
+  do {
+    var mod = index % 26;
+    name = String.fromCharCode(mod+65) + name;
+    index = Math.floor (index / 26) - 1;
+  } while (index >= 0);
+  return name;
 }
 
-function addrow () {
-  calculatorui.innerHTML += template;
-  var newelm = document.getElementById("newrow");
-  newelm.id = "row" + rowcount;
-  rowarray[rowcount] = newelm;
+function addrow (size) {
+  // Insert new row elements
+  var tr = calculatorui.insertRow(-1);
+  tr.class = "calcrow";
+  tr.innerHTML = template;
+
+  // Record element objects.
+  var row = {
+    "index": rowcount,
+    "name": rowname(rowcount),
+    "tr": tr,
+  };
+  for (var cls in elements) {
+    var elm = tr.querySelector("." + elements[cls]);
+    elm.row = row;
+    row[elements[cls]] = elm;
+  }
+
+  // Initialize new row data
+  row.labelpara.innerHTML = row.name;
+  row.sizepara.innerHTML = Math.pow(2, size+2).toString() + "-bit";
+  row.size = size;
+
+  // Save row object
+  rowarray[rowcount] = row;
   rowcount++;
-
-  var hex = newelm.querySelector(".hexbox");
 }
 
-addrow ();
+addrow (3);
